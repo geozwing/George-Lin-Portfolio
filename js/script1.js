ffe548bda44e1ceda4314b29e8af13e6cb55cf50
George <georgelin984@gmail.com>
2019-11-26 23:14:26 -0800

    Updated all pages, added hero button to hero images, updated style guide

--- js/script1.js
+++ js/script1.js
@@ -1,23 +1,8 @@
-$(document).ready(function() {
+// Hero Scroll button
 
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-
-        /* Check the location of each desired element */
-        $('.hideme').each( function(i){
-
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-
-                $(this).animate({'opacity':'1'},500);
-
-            }
-
-        });
-
-    });
-
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
 });
